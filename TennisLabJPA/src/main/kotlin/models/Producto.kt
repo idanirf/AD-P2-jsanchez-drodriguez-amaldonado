22f3704adb877814564ee949b37f05590201d808
Jorge <jsanchezberrocoso@gmail.com>
2022-12-14 21:51:40 +0100

    Repositorio Usuario Arreglado

--- TennisLabJPA/src/main/kotlin/models/Producto.kt
+++ TennisLabJPA/src/main/kotlin/models/Producto.kt
@@ -1,10 +1,11 @@
 package models
 
+import models.enums.TipoProducto
 import org.hibernate.annotations.Type
 import java.util.*
 import javax.persistence.*
 @Entity
-@Table(name = "PRODUCTO")
+@Table(name = "Producto")
 @NamedQuery(name = "Producto.findAll", query = "SELECT p FROM Producto p")
 data class Producto(
     @Id
@@ -12,27 +13,12 @@ data class Producto(
     @Column(name = "uuid")
     @Type(type = "uuid-char")
     val uuid: UUID,
-    val tipoProducto: TipoProducto,
     val marca: String,
     val modelo: String,
     val precio: Double,
     val stock: Int,
+    val tipoProducto: TipoProducto,
     @ManyToOne(cascade = [CascadeType.ALL])
     @JoinColumn(name = "pedido_id", referencedColumnName = "id", nullable = true)
     val pedido: Pedido
 )
-
-enum class TipoProducto(val tipoProducto: String) {
-    RAQUETA("RAQUETA"),
-    CORDAJE("CORDAJE"), COMPLEMENTO("COMPLEMENTO");
-    companion object {
-        fun from(producto: String): TipoProducto {
-            return when (producto.uppercase()) {
-                "RAQUETA" -> RAQUETA
-                "CORDAJE" -> CORDAJE
-                "COMPLEMENTO" -> COMPLEMENTO
-                else -> throw IllegalArgumentException("Producto no válido.")
-            }
-        }
-    }
-}
