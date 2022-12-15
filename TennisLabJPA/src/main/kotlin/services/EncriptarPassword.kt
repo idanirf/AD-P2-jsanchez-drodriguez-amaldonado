80ad8b259d5a0b84fae65b7fb2c2bd31d32257c7
Jorge <jsanchezberrocoso@gmail.com>
2022-12-15 21:27:12 +0100

    Encriptar Password

---
+++ TennisLabJPA/src/main/kotlin/services/EncriptarPassword.kt
@@ -0,0 +1,12 @@
+package services
+
+import com.google.common.hash.Hashing
+import java.nio.charset.StandardCharsets
+
+class EncriptarPassword {
+    fun encriptar(contra: String): String {
+        return Hashing.sha256()
+            .hashString(contra, StandardCharsets.UTF_8)
+            .toString()
+    }
+}
