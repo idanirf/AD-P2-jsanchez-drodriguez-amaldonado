745c5e9802a67e9103a5231cd65c0ec9ec21f77d
ALFREDO MALDONADO <alfredorafael.maldonado@alumno.iesluisvives.org>
2022-12-15 22:23:40 +0100

    Paquete repositories.Maquina completado

--- TennisLabJPA/src/main/kotlin/repositories/maquina/MaquinaEncordarRepositoryImplement.kt
+++ TennisLabJPA/src/main/kotlin/repositories/maquina/MaquinaEncordarRepositoryImplement.kt
@@ -1,21 +1,51 @@
 package repositories.maquina
 
+import db.HibernateManager
+import db.HibernateManager.manager
 import models.MaquinaEncordar
+import mu.KotlinLogging
+import javax.persistence.TypedQuery
 
-class MaquinaEncordarRepositoryImplement: IMaquinaEncordarRepository  {
+private val logger = KotlinLogging.logger {}
+
+class MaquinaEncordarRepositoryImplement : IMaquinaEncordarRepository {
     override fun findAll(): List<MaquinaEncordar> {
-        TODO("Not yet implemented")
+        logger.debug { "findAll()" }
+        var maquinas = mutableListOf<MaquinaEncordar>()
+        HibernateManager.query {
+            val query: TypedQuery<MaquinaEncordar> =
+                manager.createNamedQuery("MaquinaEncordar.findAll", MaquinaEncordar::class.java)
+            maquinas = query.resultList
+        }
+        return maquinas
     }
 
     override fun findById(id: Int): MaquinaEncordar? {
-        TODO("Not yet implemented")
+        logger.debug { "findById($id)" }
+        var maquina: MaquinaEncordar? = null
+        HibernateManager.query {
+            maquina = manager.find(MaquinaEncordar::class.java, id)
+        }
+        return maquina
     }
 
     override fun save(entity: MaquinaEncordar): MaquinaEncordar {
-        TODO("Not yet implemented")
+        logger.debug { "save($entity)" }
+        HibernateManager.transaction {
+            manager.merge(entity)
+        }
+        return entity
     }
 
     override fun delete(id: Int): Boolean {
-        TODO("Not yet implemented")
+        var res = false
+        HibernateManager.transaction {
+            val maquina = findById(id)
+            if (maquina != null) {
+                manager.remove(maquina)
+                res = true
+            }
+        }
+        return res
     }
 }
